9e5a325e05e5e905ceda77b0446bdbe9b2d93a98
kalitsinskiy <kalitsinskij.46@gmail.com>
2022-11-06 18:53:50 +0200

    add gestures to index

--- src/GestureDetector/index.js
+++ src/GestureDetector/index.js
@@ -1,9 +1,11 @@
 import GestureDetector from './GestureDetector'
 import RunButton from './components/RunButton'
+import gestures from './gestures'
 import gesturesIcons from './components/gesturesIcons'
 
 export {
     GestureDetector,
     RunButton,
+    gestures,
     gesturesIcons,
 }
